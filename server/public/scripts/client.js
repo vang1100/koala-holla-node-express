56e108f5d36b259e1bb54aa31b55fd7e92a0b5b4
Padee Vang <vangpadee11@gmail.com>
2024-03-09 12:32:59 -0600

    working on buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,11 +5,21 @@ console.log( 'js' );
 function addKoala(){
   console.log('submit koala');
   let nameInput = document.querySelector('#nameIn').value;
+  let ageInput = document.querySelector('#ageIn').value;
+  let colorInput = document.querySelector('#colorIn').value;
+  let transferInput = document.querySelector('#readyForTransferIn').value;
+  let notesInput = document.querySelector('#notesIn').value;
   let koalaData = document.querySelector('#viewKoalas');
 
   koalaData.innerHTML += `
   <tr>
     <td>${nameInput}</td>
+    <td>${ageInput}</td>
+    <td>${colorInput}</td>
+    <td>${transferInput}</td>
+    <td>${notesInput}</td>
+    <td><button onClick="markReady()">Mark Ready</button></td>
+    <td><button onClick="deleteButton()">Delete</button></td>
   </tr>
     `;
 }
