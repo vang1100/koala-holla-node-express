9ebf3e3fa9a402489f262842f7428b9496bf6d62
Padee Vang <vangpadee11@gmail.com>
2024-03-16 11:12:25 -0500

    testing router GET

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -3,8 +3,39 @@ const koalaRouter = express.Router();
 
 // DB CONNECTION
 
+const pg = require('pg');
 
-// GET
+const pool = new pg.Pool({
+    database: 'koala',
+    host: 'localhost',
+    port: 5432,
+});
+
+let koalaList = [
+    {
+        id: 10,
+        name: 'Sparkles',
+        age: 13,
+        favColor: 'red',
+        isReady: false,
+        notes: 'n/a'
+    }
+]
+
+// GET - get koala in database
+
+router.get('/', (req, res) =>{
+    let queryText = 'SELECT * FROM "koalas";';
+    pool.query(queryText)
+        .then((result) =>{
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('error in GET /koala', error);
+            res.sendStatus(500);
+        });
+});
+
 
 
 // POST
