63d9878a7facfc54f13fd32af352484fdd38b9a9
Padee Vang <vangpadee11@gmail.com>
2024-07-28 19:20:56 -0500

    made router get and post, working on saveKoala() function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,53 @@
+//const koalaRouter = require("../../routes/koala.router");
+
 console.log( 'js' );
 
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+
   
+  axios.get('/koalas').then((response) => {
+
+    //this console log should show the koalalist array from the server side
+    //console.log('response', response.data);
+
+    let koalasFromServer = response.data;
+    console.log(koalasFromServer);
+
+    let viewKoalasDiv = document.querySelector('#viewKoalas');
+    
+    //looping over response.data to put on DOM
+
+    for (let items of koalasFromServer) {
+      viewKoalasDiv.innerHTML += `
+          <tr>
+            <td>${items.name}</td>
+            <td>${items.age}</td>
+            <td>${items.color}</td>
+            <td>${items.transfer}</td>
+            <td>${items.notes}</td>
+            <td><button>Delete</button></td>
+            `
+    }
+
+  });
+
 } // end getKoalas
 
 function saveKoala(){
   console.log( 'in saveKoala' );
+   // let nameVal = document.querySelector('#nameIn').value;
+  // let ageVal = document.querySelector('#ageIn').value;
+  // let colorVal = document.querySelector('#colorIn').value;
+  // let transferStatus = document.querySelector('#readyForTransferIn').value;
+  // let notesVal = document.querySelector('#notesIn').value;
+  let viewKoalasDiv = document.querySelector('#viewKoalas');
+  viewKoalasDiv.innerHTML += `test`;
   // axios call to server to get koalas
  
 }
 
 getKoalas();
+
+// need function for delete button
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,80 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+// adding test data
+
+let koalaList = [ 
+   {
+    id: 1,
+    name: 'Scotty',
+    age: 4,
+    color: 'Red',
+    transfer: 'Y',
+    notes: 'Born in Guatemala'
+},
+{
+    id: 2,
+    name: 'Jean',
+    age: 5,
+    color: 'Green',
+    transfer: 'Y',
+    notes: 'Allergic to lots of lava'
+},
+{
+    id: 3,
+    name: 'Ororo',
+    age: 7,
+    color: 'Yellow',
+    transfer: 'N',
+    notes: 'Loves listening to Paula (Abdul)'
+},
+{
+    id: 4,
+    name: 'K Leaf',
+    age: 15,
+    color: 'Purple',
+    transfer: 'N',
+    notes: 'Never refuses a treat.'
+},
+{
+    id: 5,
+    name: 'Charlie',
+    age: 9,
+    color: 'Orange',
+    transfer: 'Y',
+    notes: 'Favorite band is Nirvana'
+},
+{
+    id: 6,
+    name: 'Betsy',
+    age: 4,
+    color: 'Blue',
+    transfer: 'Y',
+    notes: 'Has a pet iguana'
+
+},
+];
+
 // DB CONNECTION
 
 
 // GET
 
+koalaRouter.get('/', (req, res) => {
+    console.log('in GET route');
+    res.send(koalaList)
+})
 
 // POST
 
+koalaRouter.post('/', (req, res) => {
+    console.log('POST made for /koalas');
+    console.log('req.body in post', req.body);
+
+    let koalasToAdd = req.body;
+    koalaList.push(koalasToAdd);
+    res.sendStatus(201);
+})
 
 // PUT
 
